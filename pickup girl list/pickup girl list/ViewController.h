b4d0569bca3a52761c68b837e6c80210c4dd6ce8
phoenixne <phoenixne@gmail.com>
2015-03-20 23:05:53 +0800

    add pick up  girl
    增加了 风流谱

---
+++ pickup girl list/pickup girl list/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  pickup girl list
+//
+//  Created by phoenix on 3/20/15.
+//  Copyright (c) 2015 home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ pickup girl list/pickup girl list/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  pickup girl list
+//
+//  Created by phoenix on 3/20/15.
+//  Copyright (c) 2015 home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
