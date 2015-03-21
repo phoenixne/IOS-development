71a2aa7ddf31a4216d736320e78a7d33d05dd9e2
phoenixne <phoenixne@gmail.com>
2015-03-21 19:45:36 +0800

    3.4 章 按钮
    P49 页案例

---
+++ Me_Switch_slider_segmentedcontrolsample/Me_Switch_slider_segmentedcontrolsample/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  Me_Switch_slider_segmentedcontrolsample
+//
+//  Created by phoenix on 3/21/15.
+//  Copyright (c) 2015 home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UISwitch *LeftSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *RightSwitch;
+
+
+@end
+
---
+++ Me_Switch_slider_segmentedcontrolsample/Me_Switch_slider_segmentedcontrolsample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Me_Switch_slider_segmentedcontrolsample
+//
+//  Created by phoenix on 3/21/15.
+//  Copyright (c) 2015 home. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
