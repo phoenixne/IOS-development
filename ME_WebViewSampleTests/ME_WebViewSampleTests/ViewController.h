5781ac1a94838042cd494f799353e25664404cf7
phoenixne <phoenixne@gmail.com>
2015-03-22 22:18:00 +0800

    try

---
+++ ME_WebViewSampleTests/ME_WebViewSampleTests/ViewController.h
@@ -0,0 +1,20 @@
+//
+//  ViewController.h
+//  ME_WebViewSampleTests
+//
+//  Created by phoenix on 3/22/15.
+//  Copyright (c) 2015 home. All rights reserved.
+// try
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController  <UIWebViewDelegate>
+
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
+
+- (IBAction)testLoadHTMLString:(id)sender;
+- (IBAction)testLoadData:(id)sender;
+- (IBAction)testLoadRequest:(id)sender;
+
+@end
+
