a5c4194e44176585987ca4a8d7f9009a3e1a998b
phoenixne <phoenixne@gmail.com>
2015-03-24 20:49:21 +0800

    2

--- Me_MyphoneGap/Me_MyphoneGap/ViewController.m
+++ Me_MyphoneGap/Me_MyphoneGap/ViewController.m
@@ -17,11 +17,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    self.webView.delegate = self;
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"www/index" ofType:@"html"];
+    [self.webView loadRequest:[NSURLRequest requestWithURL:[NSURL fileURLWithPath: path]]];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+
+#pragma mark UIWebViewDelegate
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView
+{
+    [self.webView stringByEvaluatingJavaScriptFromString:@"helloWorld('从iOS对象中调用JS Ok.')"];
+}
+
+
+
+
+
 
 @end
