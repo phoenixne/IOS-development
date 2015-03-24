9362aeda0524e9f058e353998f3a98a74fd0d49f
phoenixne <phoenixne@gmail.com>
2015-03-24 20:06:55 +0800

    3.52 add

---
+++ Me_MyphoneGap/Me_MyphoneGap/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Me_MyphoneGap
+//
+//  Created by phoenix on 3/24/15.
+//  Copyright (c) 2015 home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Me_MyphoneGap/Me_MyphoneGap/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Me_MyphoneGap
+//
+//  Created by phoenix on 3/24/15.
+//  Copyright (c) 2015 home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
